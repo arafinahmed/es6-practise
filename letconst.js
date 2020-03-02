8b2a58405238ac428eea0b910f6a134e57db1012
arafinahmed <arafin2021@gmail.com>
2020-03-03 00:24:11 +0600

    test

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const name = "Arafin Ahmed";
+console.log(name);
+
+const numbers = [12, 45];
+numbers[1] = 20;
+numbers.push(12);
+console.log(numbers);
+
+const nayok = {name:"sakib khan", phone:123, age:20}
+console.log(nayok);
+
+let yourName = "your name";
+console.log(yourName);
+yourName = "my name";
+console.log(yourName);
+let sum = 0;
+for(let i=0; i<10; i++){
+    sum += i;
+}
+
